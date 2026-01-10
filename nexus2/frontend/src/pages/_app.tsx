2de9e921e070c34dd0f6e1debf43a65d2c0503ea
ftbboy2115 <ftbboy2115@users.noreply.github.com>
2026-01-10 16:19:48 -0500

    cleanup: Revert unnecessary mobile CSS experiments
    Reverted:- Grid back to simple minmax(300px, 1fr)- Removed .container * wildcard rule- Removed app wrapper div- Removed stat max-width/overflowKept: viewport meta, hamburger menu, table scroll fix

--- nexus2/frontend/src/pages/_app.tsx
+++ nexus2/frontend/src/pages/_app.tsx
@@ -5,12 +5,12 @@ import Navbar from '@/components/Navbar'
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <div style={{ width: '100%', minHeight: '100vh', boxSizing: 'border-box' }}>
+        <>
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
             </Head>
             <Navbar />
             <Component {...pageProps} />
-        </div>
+        </>
     )
 }
